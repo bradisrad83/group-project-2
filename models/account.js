03e5c0b280a1e89b017a01c91b9022105a6fb58b
Jose Velez <jose.velez363@gmail.com>
2017-05-17 21:01:33 -0400

    Account and Profile table created on the database, need to figure out how to make the associations work.

--- models/account.js
+++ models/account.js
@@ -67,7 +67,7 @@ module.exports = function(sequelize, DataTypes) {
         // This is for any additional configuration we want to give our models
 
         // We're saying that we want our Account to have Posts
-        classMethods: {
+      /*  classMethods: {
             associate: function (models) {
                 // Associating Account  with profile
                 // When an Account is deleted, also delete any associated profile
@@ -76,7 +76,7 @@ module.exports = function(sequelize, DataTypes) {
                 });
 
             }
-        }
+        }*/
     });
     return Account;
 };
--- models/profile.js
+++ models/profile.js
@@ -20,14 +20,14 @@ module.exports = function(sequelize, DataTypes) {
                 }
             },
             lastName:{
-                type: DataTypes.String,
+                type: DataTypes.STRING,
                 allowNull:false,
                 validate: {
                     len: [1]
                 }
             },
             location:{
-                type: DataTypes.String,
+                type: DataTypes.STRING,
                 allowNull: false,
                 validate: {
                     len: [1]
@@ -60,7 +60,7 @@ module.exports = function(sequelize, DataTypes) {
 
 
             // We're saying that we want our Author to have Posts
-            classMethods: {
+            /*classMethods: {
                 associate: function(models) {
                     // An Author (foreignKey) is required or a Post can't be made
                     Profile.belongsTo(models.Account, {
@@ -69,7 +69,7 @@ module.exports = function(sequelize, DataTypes) {
                         }
                     });
                 }
-            }
+            }*/
         }
     );
     return Profile;
