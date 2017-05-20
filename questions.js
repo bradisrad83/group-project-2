e967ab4c5e0eb25ed1e9497ab0186567d52a3992
Brad Goldsmith <b-rad@Brads-MacBook-Pro.local>
2017-05-20 12:00:12 -0400

    questions.js file created with 20 multiple choice questions.  Algorithm still needed for them

---
+++ questions.js
@@ -0,0 +1,143 @@
+var questions = [{
+    question: "What drink do you prefer?",
+    answer1: "Coffee",
+    answer2: "Cocktail/Beer",
+    answer3: "Water",
+    answer4: "Juice"
+  },
+  {
+    question: "What is your favorite Season?",
+    answer1: "Summer",
+    answer2: "Winter",
+    answer3: "Spring",
+    answer4: "Fall"
+  },
+  {
+    question: "What genre of movies do you prefer?",
+    answer1: "Comdedy",
+    answer2: "Action/Thriller",
+    answer3: "Horror",
+    answer4: "Fantasy"
+  },
+  {
+    question: "Which food do you prefer?",
+    answer1: "Pizza",
+    answer2: "Tacos",
+    answer3: "Steak",
+    answer4: "Vegetarian"
+  },
+  {
+    question: "Which type of person are you?",
+    answer1: "20 Minutes early",
+    answer2: "Right on time",
+    answer3: "5 Minutes late",
+    answer4: "Whoops, my bad"
+  },
+  {
+    question: "What type of music do you prefer?",
+    answer1: "Rock",
+    answer2: "Country",
+    answer3: "R&B",
+    answer4: "Hip-hop",
+  },
+  {
+    question: "Which color do you like best?",
+    answer1: "Blue",
+    answer2: "Green",
+    answer3: "Red",
+    answer4: "White"
+  },
+  {
+    question: "What kind of date do you prefer?",
+    answer1: "Dinner and a movie",
+    answer2: "Converstation over coffee/drinks",
+    answer3: "Afternoon hike",
+    answer4: "People watching"
+  },
+  {
+    question: "What is your favorite pizza topping?",
+    answer1: "Pepperoni",
+    answer2: "Pineapple",
+    answer3: "Cheese",
+    answer4: "I dont like pizza"
+  },
+  {
+    question: "What is your favorite holiday?",
+    answer1: "Thanksgiving",
+    answer2: "Halloween",
+    answer3: "Christmas",
+    answer4: "St. Paddys Day"
+  },
+  {
+    question: "What is your ideal vacation?",
+    answer1: "Cabin in the woods",
+    answer2: "Visiting a big city",
+    answer3: "Road trip",
+    answer4: "Throw a dart at a map and boom"
+  },
+  {
+    question: "What kind of vehicle do you prefer?",
+    answer1: "SUV",
+    answer2: "Sedan",
+    answer3: "Truck",
+    answer4: "Sporty Coupe"
+  },
+  {
+    quesiton: "How many tattoos do you have?",
+    answer1: "0",
+    answer2: "1-5",
+    answer3: "6-10",
+    answer4: "I stopped counting"
+  },
+  {
+    question: "What kind of pet do you prefer?",
+    answer1: "Dog",
+    answer2: "Cat",
+    answer3: "Reptiles",
+    answer4: "Birds",
+    answer5: "Not a fan"
+  },
+  {
+    question: "Whats the age of your ideal match?",
+    answer1: "18-25",
+    answer2: "26-32",
+    answer3: "33-40",
+    answer4: "40+"
+  },
+  {
+    question: "What is your stance on kids",
+    answer1: "I have them",
+    answer2: "I want them",
+    answer3: "I don't want them",
+    answer4: "I don't know"
+  },
+  {
+    question: "What is your relationship with your family?",
+    answer1: "We are super close and talk almost everyday",
+    answer2: "We are close and talk about once a week",
+    answer3: "I love them but we don't talk all that often",
+    answer4: "I have no one I consider family"
+  },
+  {
+    question: "Which do you wear the most?",
+    answer1: "Sandals",
+    answer2: "Gym shoes",
+    answer3: "Work boots",
+    answer4: "Dress shoes"
+  },
+  {
+    question: "Where do you see yourself in ten years?",
+    answer1: "Same job same city",
+    answer2: "Same job new state",
+    answer3: "Retired and traveling",
+    answer4: "New career"
+  },
+  {
+    question: "How much television do you watch in a week?",
+    answer1: "0-5 hours",
+    answer2: "6-11 hours",
+    answer3: "11-16 hours",
+    answer4: "17-22 hours",
+    answer5: "23+ hours"
+  }
+];
