508f355d4c22bc0a80e2d48f141704e2707b7171
jasonboru <jasonboru@gmail.com>
2017-05-21 07:44:16 -0400

    added a second modal for the register post function

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,4 +2,14 @@ $(document).ready(function() {
   $(".clickLog").click(function() {
     $('#logInModal').modal('show');
   });
+
+  $('#getRegModal').click(function() {
+    $('#logInModal').modal('hide');
+    $('#registerModal').modal('show');
+  });
+
+  $('#getLogin').click(function() {
+    $('#registerModal').modal('hide');
+    $('#logInModal').modal('show');
+  });
 })
