7634cb4d4af1871417d0ba5b9a7e36dfb547cc10
willisdorden <willisdorden@gmail.com>
2017-05-27 14:42:35 -0400

    have post working

--- controllers/profile-api.js
+++ controllers/profile-api.js
@@ -1,4 +1,4 @@
-var db = require("../models");
+var db = require("../models/index");
 
 module.exports = function(app){
   // Route to get all the profile and send the json to the api route
@@ -9,10 +9,11 @@ module.exports = function(app){
   });
   // Route to post the new profile to the table
   app.post("/api/profile", function(req, res){
+    console.log(req.body);
     db.Profile.create({
-      Username: req.body.Username,
       firstName: req.body.firstName,
       lastName: req.body.lastName,
+         sex: req.body.sex,
       location: req.body.location,
       zipCode: req.body.zipCode,
       bio: req.body.bio,
@@ -20,7 +21,7 @@ module.exports = function(app){
     }).then(function(dbProfile){
       res.json(dbProfile);
     // Catch the error and display it on the console 
-    }).catch(error => {
+    }).catch(function(error) {
       console.log(error);
       res.status(500).json(error);
     });
