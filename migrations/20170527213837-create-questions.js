8cdc68b488da2a87e1e431a42a8dafcc13301a47
jasonboru <jasonboru@gmail.com>
2017-05-27 23:22:10 -0400

    adding styling and some functionality to the survey

--- migrations/20170527213837-create-questions.js
+++ migrations/20170527213837-create-questions.js
@@ -16,6 +16,10 @@ module.exports = {
         allowNull: false,
         type: Sequelize.STRING
       },
+      qId: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
       answer1: {
         allowNull: false,
         type: Sequelize.STRING
@@ -39,6 +43,7 @@ module.exports = {
     }).then(function() {
       Questions.bulkCreate([{
         question: "Your friend calls and says let's go grab a beer.  What do you do?",
+        qId: "q1",
         answer1: "You know your buddy is a drinker and don't answer the phone.",
         answer2: "You answer the phone but make up an excuse.",
         answer3: "You go out but only for a drink or two beer.",
@@ -46,6 +51,7 @@ module.exports = {
         answer5: "You go out, drink all night, and pay the entire tab."
       }, {
         question: "How do you feel about snow, and winter in general?",
+        qId: "q2",
         answer1: "Not a chance, I don't even own a jacket.",
         answer2: "I love seeing it on the internet and TV.",
         answer3: "I don't mind seeing it on vacation but prefer not to live in it.",
@@ -53,6 +59,7 @@ module.exports = {
         answer5: "I love it and want a blizzard every other weekend."
       }, {
         question: "A horror movie comes on, what do you do?",
+        qId: "q3",
         answer1: "Turn that crap off now!!!!",
         answer2: "I guess if there is nothing better on",
         answer3: "As long as it's not gory and disgusting, we're good",
@@ -60,6 +67,7 @@ module.exports = {
         answer5: "The scarier the better."
       }, {
         question: "What's your stance on the great pineapple pizza debate?",
+        qId: "q4",
         answer1: "Absolutely not",
         answer2: "Once in a while, sure why not",
         answer3: "Yes but can we add more than just pineapple?",
@@ -67,6 +75,7 @@ module.exports = {
         answer5: "There can never be enough pineapple."
       }, {
         question: "Are you an person whom is always on time?",
+        qId: "q5",
         answer1: "I always run late :(",
         answer2: "50% of the time I'm always on time",
         answer3: "For the most part yes",
@@ -74,6 +83,7 @@ module.exports = {
         answer5: "I'd rather be an hour early than 5 minutes late."
       }, {
         question: "Country music, and go.",
+        qId: "q6",
         answer1: "Please no",
         answer2: "Are we talking old school Johnny Cash or moder pop country?",
         answer3: "Some music is better than no music",
@@ -81,6 +91,7 @@ module.exports = {
         answer5: "I usually wear cowboy hats and boots"
       }, {
         question: "Is blue your favorite color?",
+        qId: "q7",
         answer1: "Gross",
         answer2: "Only in eyes, other than that no",
         answer3: "Blue is cool I guess",
@@ -88,6 +99,7 @@ module.exports = {
         answer5: "I was a smurf in another life"
       }, {
         question: "How do you feel about a hike for a date?",
+        qId: "q8",
         answer1: "Oh no I'm not the outdoorsy type at all",
         answer2: "As long as it doesn't involve climbing",
         answer3: "Sounds fun, I'll give it a try",
@@ -95,6 +107,7 @@ module.exports = {
         answer5: "When do we leave!!!!!!!!!!!"
       }, {
         question: "How do you feel about Halloween?",
+        qId: "q9",
         answer1: "The devils holiday",
         answer2: "I leave a bucket outside for the nieghborhood kids",
         answer3: "I'm in it for the candy",
@@ -102,6 +115,7 @@ module.exports = {
         answer5: "Let's do matching over the top costumes, decorate the house, the whole nine yards"
       }, {
         question: "You have a week off, road trip?",
+        qId: "q10",
         answer1: "I don't even own a car",
         answer2: "Maybe for a day or so but not the whole week",
         answer3: "As long as I don't have anything better to do",
@@ -109,6 +123,7 @@ module.exports = {
         answer5: "Grab and bag and let's see where we end up!!!!!!"
       }, {
         question: "Would you drive/own a hybrid/electric car?",
+        qId: "q11",
         answer1: "I own a diesel truck and will not be getting rid of it anytime soon",
         answer2: "Not feeling it, nope",
         answer3: "I think I'm ready for that MPG",
@@ -116,6 +131,7 @@ module.exports = {
         answer5: "Already looking for my second one"
       }, {
         question: "Could you live in a tiny house?",
+        qId: "q12",
         answer1: "Way to cramped for my style",
         answer2: "Maybe, but I think I have way too much stuff",
         answer3: "It's crossed my mind a time or two",
@@ -123,6 +139,7 @@ module.exports = {
         answer5: "I already am, or planning to in the near future"
       }, {
         question: "How close are you with your family?",
+        qId: "q13",
         answer1: "My family is dead to me",
         answer2: "I see them once a year or so",
         answer3: "I wish I could see them more",
@@ -130,6 +147,7 @@ module.exports = {
         answer5: "I see them all the time and cannot spend enough time with them"
       }, {
         question: "How do you feel about sandals?",
+        qId: "q14",
         answer1: "Feet are gross please cover them up",
         answer2: "As long as it's not cold",
         answer3: "If I'm going to the beach sure",
@@ -137,6 +155,7 @@ module.exports = {
         answer5: "I don't even own shows, sandals all the way"
       }, {
         question: "How do you feel about kids?",
+        qId: "q15",
         answer1: "No thank you",
         answer2: "As long as their not mine",
         answer3: "Maybe, maybe not, just kinda indifferent right now",
@@ -144,6 +163,7 @@ module.exports = {
         answer5: "I have them and or want them ASAP"
       }, {
         question: "In ten years do you see yourself in the same career/location?",
+        qId: "q16",
         answer1: "Please no, I don't know if I could handle it anymore",
         answer2: "I can't think that far ahead",
         answer3: "Love where I live but it's time for a career change",
@@ -151,6 +171,7 @@ module.exports = {
         answer5: "I love my job and city and wouldn't be happier"
       }, {
         question: "Do you watch a lot of television?",
+        qId: "q17",
         answer1: "I don't own a tv",
         answer2: "It's on but solely as background noise",
         answer3: "I keep up with a few shows here and there",
@@ -158,6 +179,7 @@ module.exports = {
         answer5: "Guilty as charged, it's always on and I'm always watching"
       }, {
         question: "Are pets family?",
+        qId: "q18",
         answer1: "I don't like animals",
         answer2: "No they are just pets",
         answer3: "Pets are ok",
@@ -165,6 +187,7 @@ module.exports = {
         answer5: "Mine my as well by my children, and I'm always looking to expand my family"
       }, {
         question: "Are you a tattoo person?",
+        qId: "q19",
         answer1: "Why defile your body?",
         answer2: "I have a few but don't plan on anymore in the future",
         answer3: "I have a few and plan a couple more in the future",
@@ -172,6 +195,7 @@ module.exports = {
         answer5: "I want a full body suit eventually"
       }, {
         question: "Do you like to read?",
+        qId: "q20",
         answer1: "Haven't read a book since high school",
         answer2: "Did they make it into a movie?",
         answer3: "Yes but it has to be the right book",
